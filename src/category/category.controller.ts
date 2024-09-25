f68b7631b2dcb37955340f542396c25d9d891dcb
bek <shoyatbekov03032003@gmail.com>
2024-09-25 17:38:55 +0500

    category module completed

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { Category, Prisma } from '@prisma/client';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) { }
+
+
+  @Post()
+  async create(@Body() createCategoryDto: Prisma.CategoryCreateInput): Promise<Category> {
+    return this.categoryService.create(createCategoryDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Category[]> {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Category> {
+    return this.categoryService.findOne(+id);
+  }
+
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateCategoryDto: Prisma.CategoryUpdateInput): Promise<Category> {
+    return this.categoryService.update(+id, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<Category> {
+    return this.categoryService.remove(+id);
+  }
 }
