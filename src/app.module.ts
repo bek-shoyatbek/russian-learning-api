64f19486ebed0236e5568336de15629c1bfab66e
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-24 16:21:05 +0500

    feat: section module completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { CategoryModule } from './category/category.module';
 import { RewardModule } from './reward/reward.module';
+import { SectionModule } from './section/section.module';
 
 @Module({
   imports: [UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
     CategoryModule,
-    RewardModule
+    RewardModule,
+    SectionModule
   ],
   controllers: [AppController],
   providers: [AppService],
