81b6fd343eff1dd48ae58e893aece2d6ad96c327
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-28 11:51:16 +0500

    feat: morgan deleted for now

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as morgan from 'morgan';
 import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
 
 async function bootstrap() {
@@ -23,7 +22,6 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   // Middlewares
-  app.use(morgan('dev'));
   app.enableCors({origin: "*"});
   app.useGlobalFilters(new PrismaExceptionFilter());
 
